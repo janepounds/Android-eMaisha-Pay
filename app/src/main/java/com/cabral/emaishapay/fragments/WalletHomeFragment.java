b174e37f197a09500738f00991c4bb9ca886d741
ONENEMMANUEL <osoema.9@gmail.com>
2020-12-04 17:39:57 +0300

    Implemented homepage functionality

--- app/src/main/java/com/cabral/emaishapay/fragments/WalletHomeFragment.java
+++ app/src/main/java/com/cabral/emaishapay/fragments/WalletHomeFragment.java
@@ -8,16 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -28,11 +23,14 @@ import androidx.navigation.Navigation;
 import com.cabral.emaishapay.DailogFragments.Buy;
 import com.cabral.emaishapay.DailogFragments.TransferMoney;
 import com.cabral.emaishapay.R;
-import com.cabral.emaishapay.activities.WalletHomeActivity;
 import com.cabral.emaishapay.activities.WalletAuthActivity;
+import com.cabral.emaishapay.activities.WalletHomeActivity;
+import com.cabral.emaishapay.databinding.EmaishaPayHomeBinding;
 import com.cabral.emaishapay.models.BalanceResponse;
 import com.cabral.emaishapay.network.APIClient;
 import com.cabral.emaishapay.network.APIRequests;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.text.NumberFormat;
 
@@ -42,69 +40,33 @@ import retrofit2.Response;
 
 public class WalletHomeFragment extends Fragment {
     private static final String TAG = "WalletHomeFragment";
+    private EmaishaPayHomeBinding binding;
     private Context context;
     private NavController navController = null;
-
-    public static double balance = 0;
-    ActionBar actionBar;
-    public static FragmentManager fm;
-
-    //Imageucf
     private ProgressDialog progressDialog;
 
-    Toolbar toolbar;
-    TextView walletBalance, usernameWalletHome;
-    LinearLayout layoutWalletTransfer, layoutWalletTransactions, layoutWalletLoans, layoutWalletCoupons, layoutWalletBuy;
-    ImageView btnWalletDeposit, btnWalletTransfer, btnWalletTransactions, btnWalletLoans, btnWalletCoupons, btnWalletBuy;
+    public static double balance = 0;
+    public static FragmentManager fm;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.emaisha_pay_home, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.emaisha_pay_home, container, false);
 
-//        progressDialog = new ProgressDialog(context);
-//        progressDialog.setIndeterminate(true);
-//        progressDialog.setMessage("Please Wait..");
-//        progressDialog.setCancelable(false);
-//
-//        toolbar = view.findViewById(R.id.toolbar_wallet_home);
-//        walletBalance = view.findViewById(R.id.wallet_balance);
-//        usernameWalletHome = view.findViewById(R.id.username_wallet_home);
-//        btnWalletDeposit = view.findViewById(R.id.btn_wallet_deposit);
-//        layoutWalletTransfer = view.findViewById(R.id.layout_wallet_transfer);
-//        layoutWalletTransactions = view.findViewById(R.id.layout_wallet_transactions);
-//        layoutWalletLoans = view.findViewById(R.id.layout_wallet_loans);
-//        layoutWalletCoupons = view.findViewById(R.id.layout_wallet_coupons);
-//        layoutWalletBuy = view.findViewById(R.id.layout_wallet_buy);
-//        btnWalletTransfer = view.findViewById(R.id.btn_wallet_transfer);
-//        btnWalletTransactions = view.findViewById(R.id.btn_wallet_transactions);
-//        btnWalletLoans = view.findViewById(R.id.btn_wallet_loans);
-//        btnWalletCoupons = view.findViewById(R.id.btn_wallet_coupons);
-//        btnWalletBuy = view.findViewById(R.id.btn_wallet_buy);
-//
-//        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
-//        setHasOptionsMenu(true);
-//
-//        actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
-//        assert actionBar != null;
-//        actionBar.setTitle(getResources().getString(R.string.my_wallet));
-//
-//        actionBar.setHomeButtonEnabled(true);
-//        actionBar.setDisplayHomeAsUpEnabled(true);
-//
-//        fm = requireActivity().getSupportFragmentManager();
-//
-//        walletBalance.setText("UGX " + NumberFormat.getInstance().format(balance));
-//
-//        updateBalance();
-//
-//        usernameWalletHome.setText(ucf(WalletHomeActivity.getPreferences(WalletHomeActivity.PREFERENCES_LAST_NAME, context)) + " " + ucf(WalletHomeActivity.getPreferences(WalletHomeActivity.PREFERENCES_FIRST_NAME, context)));
-//
-//        Log.d(TAG, "onCreateView: Name = " + ucf(WalletHomeActivity.getPreferences(WalletHomeActivity.PREFERENCES_LAST_NAME, context)) + " " + ucf(WalletHomeActivity.getPreferences(WalletHomeActivity.PREFERENCES_FIRST_NAME, context)));
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Please Wait..");
+        progressDialog.setCancelable(false);
 
-        // return view;
-        return view;
+        fm = requireActivity().getSupportFragmentManager();
+
+        binding.walletBalance.setText("UGX " + NumberFormat.getInstance().format(balance));
+        updateBalance();
+
+        binding.username.setText("Hi, " + ucf(WalletHomeActivity.getPreferences(WalletHomeActivity.PREFERENCES_LAST_NAME, context)) + " " + ucf(WalletHomeActivity.getPreferences(WalletHomeActivity.PREFERENCES_FIRST_NAME, context)));
+
+        return binding.getRoot();
     }
 
     @Override
@@ -112,23 +74,16 @@ public class WalletHomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
 
-//        toolbar.setNavigationOnClickListener(v -> requireActivity().onBackPressed());
-//
-//        Bundle bundle = new Bundle();
-//        bundle.putDouble("balance", balance);
-//
+        Bundle bundle = new Bundle();
+        bundle.putDouble("balance", balance);
+
 //        btnWalletDeposit.setOnClickListener(view19 -> navController.navigate(R.id.action_walletHomeFragment_to_depositPayments, bundle));
-//        layoutWalletTransfer.setOnClickListener(view110 -> openTransfer());
 //        layoutWalletTransactions.setOnClickListener(view111 -> navController.navigate(R.id.action_walletHomeFragment_to_walletTransactionsListFragment));
-//        layoutWalletLoans.setOnClickListener(view112 -> navController.navigate(R.id.action_walletHomeFragment_to_walletLoansListFragment));
-//        layoutWalletCoupons.setOnClickListener(view18 -> comingSoon());
-//        layoutWalletBuy.setOnClickListener(view17 -> openBuy());
-//        btnWalletDeposit.setOnClickListener(view16 -> navController.navigate(R.id.action_walletHomeFragment_to_depositPayments, bundle));
-//        btnWalletTransfer.setOnClickListener(view15 -> openTransfer());
 //        btnWalletTransactions.setOnClickListener(view14 -> navController.navigate(R.id.action_walletHomeFragment_to_walletTransactionsListFragment));
-//        btnWalletLoans.setOnClickListener(view13 -> navController.navigate(R.id.action_walletHomeFragment_to_walletLoansListFragment));
-//        btnWalletCoupons.setOnClickListener(view12 -> comingSoon());
-//        btnWalletBuy.setOnClickListener(view1 -> openBuy());
+        binding.layoutTransfer.setOnClickListener(view110 -> openTransfer());
+        binding.layoutTopUp.setOnClickListener(view16 -> navController.navigate(R.id.action_walletHomeFragment_to_depositPayments, bundle));
+        binding.layoutLoan.setOnClickListener(view13 -> navController.navigate(R.id.action_walletHomeFragment_to_walletLoansListFragment));
+        binding.layoutPay.setOnClickListener(view1 -> openBuy());
     }
 
     @Override
@@ -154,8 +109,8 @@ public class WalletHomeFragment extends Fragment {
         ft.addToBackStack(null);
 
         // Create and show the dialog.
-        DialogFragment transferDailog = new TransferMoney(context, balance, getActivity().getSupportFragmentManager());
-        transferDailog.show(ft, "dialog");
+        DialogFragment transferDialog = new TransferMoney(context, balance, getActivity().getSupportFragmentManager());
+        transferDialog.show(ft, "dialog");
     }
 
     public void updateBalance() {
@@ -166,7 +121,7 @@ public class WalletHomeFragment extends Fragment {
         Call<BalanceResponse> call = apiRequests.requestBalance(access_token);
         call.enqueue(new Callback<BalanceResponse>() {
             @Override
-            public void onResponse(Call<BalanceResponse> call, Response<BalanceResponse> response) {
+            public void onResponse(@NotNull Call<BalanceResponse> call, @NotNull Response<BalanceResponse> response) {
                 progressDialog.dismiss();
 
                 if (response.code() == 200) {
@@ -174,7 +129,7 @@ public class WalletHomeFragment extends Fragment {
 
                     Log.d(TAG, "onSuccess: Balance = " + balance);
 
-                    walletBalance.setText("UGX " + NumberFormat.getInstance().format(balance));
+                    binding.walletBalance.setText("UGX " + NumberFormat.getInstance().format(balance));
 
                 } else if (response.code() == 401) {
                     Toast.makeText(context, "Session Expired", Toast.LENGTH_LONG).show();
@@ -186,7 +141,7 @@ public class WalletHomeFragment extends Fragment {
             }
 
             @Override
-            public void onFailure(Call<BalanceResponse> call, Throwable t) {
+            public void onFailure(@NotNull Call<BalanceResponse> call, @NotNull Throwable t) {
                 progressDialog.dismiss();
                 Log.e("info : ", new String(String.valueOf(t.getMessage())));
                 Toast.makeText(context, "An error occurred Try again Later", Toast.LENGTH_LONG).show();
@@ -196,16 +151,16 @@ public class WalletHomeFragment extends Fragment {
     }
 
     public void openBuy() {
-        FragmentTransaction ft = this.fm.beginTransaction();
-        Fragment prev = this.fm.findFragmentByTag("dialog");
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment prev = fm.findFragmentByTag("dialog");
         if (prev != null) {
             ft.remove(prev);
         }
         ft.addToBackStack(null);
 
         // Create and show the dialog.
-        DialogFragment buyfoodDailog = new Buy(context, getActivity().getSupportFragmentManager());
-        buyfoodDailog.show(ft, "dialog");
+        DialogFragment buyFoodDialog = new Buy(context, requireActivity().getSupportFragmentManager());
+        buyFoodDialog.show(ft, "dialog");
     }
 
     public void comingSoon() {
