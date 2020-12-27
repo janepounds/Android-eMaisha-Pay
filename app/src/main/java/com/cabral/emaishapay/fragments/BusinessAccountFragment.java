088bda5a9a77d37dec81714dd7dabbfce8af5a89
Georgia Tusiime <tusigeorgia.gt@gmail.com>
2020-12-27 18:31:09 +0300

    Changes made to home for business account home, added business account to account fragment and created fragment to capture agent/merchant/both fragment

---
+++ app/src/main/java/com/cabral/emaishapay/fragments/BusinessAccountFragment.java
@@ -0,0 +1,55 @@
+package com.cabral.emaishapay.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cabral.emaishapay.R;
+import com.cabral.emaishapay.databinding.FragmentBusinessAccountBinding;
+import com.cabral.emaishapay.databinding.FragmentBusinessInformationBinding;
+
+public class BusinessAccountFragment extends Fragment {
+    private FragmentBusinessAccountBinding binding;
+    private NavController navController = null;
+    public BusinessAccountFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_business_account, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
+
+
+
+    }
+}
