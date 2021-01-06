95d2ca371ebfece082158690bd5d49da3160f6a7
janepounds <janepounds1@gmail.com>
2021-01-06 17:35:12 +0300

    created top deals fragment

---
+++ app/src/main/java/com/cabral/emaishapay/fragments/buyandsell/TopDealsFragment.java
@@ -0,0 +1,13 @@
+package com.cabral.emaishapay.fragments.buyandsell;
+
+import androidx.fragment.app.Fragment;
+
+import com.cabral.emaishapay.models.product_model.ProductDetails;
+
+import java.util.List;
+
+public class TopDealsFragment extends Fragment {
+
+    public TopDealsFragment(List<ProductDetails> productDetails) {
+    }
+}
