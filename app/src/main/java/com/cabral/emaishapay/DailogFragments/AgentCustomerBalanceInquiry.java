caf46bd8e76ea29bfd4ab680692e24af6a328a9d
Georgia Tusiime <tusigeorgia.gt@gmail.com>
2021-01-09 12:50:22 +0300

    Edited withdraw and balance inquiry dialog, added withdraw details dialog, added toolbar to wallet home and edited the bottom nav bar menu item texts

--- app/src/main/java/com/cabral/emaishapay/DailogFragments/AgentCustomerBalanceInquiry.java
+++ app/src/main/java/com/cabral/emaishapay/DailogFragments/AgentCustomerBalanceInquiry.java
@@ -16,9 +16,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.cabral.emaishapay.R;
@@ -26,7 +28,8 @@ import com.flutterwave.raveutils.verification.RaveVerificationUtils;
 
 
 public class AgentCustomerBalanceInquiry extends DialogFragment {
-    LinearLayout layoutAddMoney;
+    LinearLayout layoutWalletNumber,layoutAccountNumber,layoutPhoneNumber;
+    Spinner spAccountType;
     Button addMoneyImg;
     TextView addMoneyTxt, phoneNumberTxt, errorMsgTxt;
     static String PENDING_DEPOSIT_REFERENCE_NUMBER;
@@ -58,14 +61,54 @@ public class AgentCustomerBalanceInquiry extends DialogFragment {
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
         View view = inflater.inflate(R.layout.dialog_agent_customer_balance_inquiry, null);
+        spAccountType = view.findViewById(R.id.sp_account_type);
+        layoutWalletNumber = view.findViewById(R.id.layout_balance_inquiry_wallet_no);
+        layoutAccountNumber = view.findViewById(R.id.layout_balance_inquiry_card_account_no);
+        layoutPhoneNumber = view.findViewById(R.id.layout_balance_inquiry_card_phone_no);
+
+        spAccountType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                try {
+                    //Change selected text color
+                    ((TextView) view).setTextColor(getResources().getColor(R.color.textColor));
+                } catch (Exception e) {
+
+                }
+                if(position==0){
+                    layoutWalletNumber.setVisibility(View.GONE);
+                    layoutAccountNumber.setVisibility(View.GONE);
+                    layoutPhoneNumber.setVisibility(View.GONE);
+                }
+                else if(position==1){
+                    layoutWalletNumber.setVisibility(View.VISIBLE);
+                    layoutAccountNumber.setVisibility(View.GONE);
+                    layoutPhoneNumber.setVisibility(View.GONE);
+                }
+                else if(position==2) {
+                    layoutWalletNumber.setVisibility(View.GONE);
+                    layoutAccountNumber.setVisibility(View.VISIBLE);
+                    layoutPhoneNumber.setVisibility(View.VISIBLE);
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         builder.setView(view);
-
+        Dialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(false);
+        setCancelable(false);
 
         ImageView close = view.findViewById(R.id.agent_deposit_money_close);
         close.setOnClickListener(v -> dismiss());
 
-        return builder.create();
+        return dialog;
 
     }
 }
