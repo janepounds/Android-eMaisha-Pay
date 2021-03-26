bbd65d4580e396108698b0ac360a725512f70944
janepounds <janepounds1@gmail.com>
2021-03-26 15:50:47 +0300

    implemented delete product from local database

--- app/src/main/java/com/cabral/emaishapay/DailogFragments/shop/ProductPreviewDialog.java
+++ app/src/main/java/com/cabral/emaishapay/DailogFragments/shop/ProductPreviewDialog.java
@@ -3,6 +3,7 @@ package com.cabral.emaishapay.DailogFragments.shop;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,12 +11,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.cabral.emaishapay.R;
+import com.cabral.emaishapay.activities.WalletHomeActivity;
+import com.cabral.emaishapay.database.DbHandlerSingleton;
+import com.cabral.emaishapay.fragments.CardListFragment;
+import com.cabral.emaishapay.fragments.shop_fragment.ShopProductsFragment;
 import com.cabral.emaishapay.models.WalletTransactionResponse;
 import com.cabral.emaishapay.singletons.WalletSettingsSingleton;
 
@@ -28,12 +36,15 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import es.dmoral.toasty.Toasty;
+
 public class ProductPreviewDialog extends DialogFragment {
 
     TextView produce_title_txt, product_manufacturer_txt, product_category_txt, product_code_txt, product_sell_price_txt, product_purchase_price_txt, product_stock_txt;
     private Context context;
     private HashMap<String, String>  productData;
     Button delete_button;
+    DbHandlerSingleton dbHandler;
 
    public ProductPreviewDialog(HashMap<String, String> productData){
        this.productData =productData;
@@ -82,6 +93,46 @@ public class ProductPreviewDialog extends DialogFragment {
         delete_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //call delete method
+                dbHandler = DbHandlerSingleton.getHandlerInstance(context);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage(R.string.want_to_delete_product)
+                        .setCancelable(false)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+
+                                boolean deleteProduct = dbHandler.deleteProduct(productData.get("unique_id"));
+
+                                if (deleteProduct) {
+                                    Toasty.error(context, R.string.product_deleted, Toast.LENGTH_SHORT).show();
+
+                                   //redirect to product List
+                                    getActivity().getSupportFragmentManager().popBackStack();
+
+                                    Fragment fragment = new ShopProductsFragment();
+                                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                                    fragmentManager.beginTransaction()
+                                            .hide(((WalletHomeActivity) getActivity()).currentFragment)
+                                            .replace(R.id.wallet_home_container, fragment)
+                                            .addToBackStack(null).commit();
+
+
+                                } else {
+                                    Toast.makeText(context, R.string.failed, Toast.LENGTH_SHORT).show();
+                                }
+                                dialog.cancel();
+
+                            }
+                        })
+                        .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Perform Your Task Here--When No is pressed
+                                dialog.cancel();
+                            }
+                        }).show();
+
 
             }
         });
