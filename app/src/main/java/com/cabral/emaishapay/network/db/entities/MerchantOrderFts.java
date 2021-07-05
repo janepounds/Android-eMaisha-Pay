a490fb7e43513321c138c49a9049b4ad76c71a99
DonJovan <mutesasirajovan@gmail.com>
2021-07-05 12:43:33 +0300

    updated  with migrations of virtual tables(fts4) to DB version 3

--- app/src/main/java/com/cabral/emaishapay/network/db/entities/MerchantOrderFts.java
+++ app/src/main/java/com/cabral/emaishapay/network/db/entities/MerchantOrderFts.java
@@ -16,22 +16,22 @@ public class MerchantOrderFts {
     private String order_type;
     private String order_payment_method;
     private String order_status;
-//    private String customer_name;
-//    private String customer_address;
-//    private String customer_cell;
-//    private String customer_email;
+    private String customer_name;
+    private String customer_address;
+    private String customer_cell;
+    private String customer_email;
 
     public MerchantOrderFts(int rowId, String order_date, String order_type, String order_payment_method,
-                            String order_status/*, String customer_name, String customer_address, String customer_cell, String customer_email */) {
+                            String order_status, String customer_name, String customer_address, String customer_cell, String customer_email) {
         this.rowId = rowId;
         this.order_date = order_date;
         this.order_type = order_type;
         this.order_payment_method = order_payment_method;
         this.order_status = order_status;
-//        this.customer_name = customer_name;
-//        this.customer_address = customer_address;
-//        this.customer_cell = customer_cell;
-//        this.customer_email = customer_email;
+        this.customer_name = customer_name;
+        this.customer_address = customer_address;
+        this.customer_cell = customer_cell;
+        this.customer_email = customer_email;
     }
 
     public int getRowId() {
@@ -69,36 +69,36 @@ public class MerchantOrderFts {
     public void setOrder_status(String order_status) {
         this.order_status = order_status;
     }
-//
-//    public String getCustomer_name() {
-//        return customer_name;
-//    }
-//
-//    public void setCustomer_name(String customer_name) {
-//        this.customer_name = customer_name;
-//    }
-//
-//    public String getCustomer_address() {
-//        return customer_address;
-//    }
-//
-//    public void setCustomer_address(String customer_address) {
-//        this.customer_address = customer_address;
-//    }
-//
-//    public String getCustomer_cell() {
-//        return customer_cell;
-//    }
-//
-//    public void setCustomer_cell(String customer_cell) {
-//        this.customer_cell = customer_cell;
-//    }
-//
-//    public String getCustomer_email() {
-//        return customer_email;
-//    }
-//
-//    public void setCustomer_email(String customer_email) {
-//        this.customer_email = customer_email;
-//    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getCustomer_address() {
+        return customer_address;
+    }
+
+    public void setCustomer_address(String customer_address) {
+        this.customer_address = customer_address;
+    }
+
+    public String getCustomer_cell() {
+        return customer_cell;
+    }
+
+    public void setCustomer_cell(String customer_cell) {
+        this.customer_cell = customer_cell;
+    }
+
+    public String getCustomer_email() {
+        return customer_email;
+    }
+
+    public void setCustomer_email(String customer_email) {
+        this.customer_email = customer_email;
+    }
 }
